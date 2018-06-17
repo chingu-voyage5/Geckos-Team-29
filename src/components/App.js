95a3ab42cdb8bbfd00798b6a75873a61786522e6
Erol Aliyev <aliyeverol98@gmail.com>
2018-06-17 15:06:58 +0200

    Create new setup

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const App = (props) => (
+  <p>Hello, Geckos Team 29!</p>
+);
+
+export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import 'normalize.css';
+import './styles/styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(< App/>, document.getElementById('root'));
